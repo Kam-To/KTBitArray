fce3b38f3408afb6ecfa94384ee82a5342129540
Kam <kamtoto.au@gmail.com>
2021-07-02 20:55:30 +0800

    Initial Commit

---
+++ KTBitArray/KTBitArray.h
@@ -0,0 +1,12 @@
+//
+//  KTBitArray.h
+//  KTBitArray
+//
+//  Created by Kam on 2021/7/2.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KTBitArray : NSObject
+
+@end
---
+++ KTBitArray/KTBitArray.m
@@ -0,0 +1,12 @@
+//
+//  KTBitArray.m
+//  KTBitArray
+//
+//  Created by Kam on 2021/7/2.
+//
+
+#import "KTBitArray.h"
+
+@implementation KTBitArray
+
+@end
